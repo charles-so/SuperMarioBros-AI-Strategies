0a06ba95050ab91614fe52e63546ad199ff96310
Charles So <ngolam.charles.so@gmail.com>
2023-08-25 09:24:14 +0800

    fixed update

--- environments/install.sh
+++ environments/install.sh
@@ -14,21 +14,36 @@ get_architecture() {
 # Detect the architecture
 architecture=$(get_architecture)
 
+echo "Updating conda..."
+echo
+
 # update conda
 conda update conda
+echo
 
 # Check if architecture is Intel
 if [ "$architecture" == "x86_64" ]; then
     echo "Detected Intel architecture. Creating Intel conda environment."
+    echo
     conda env create -n mario_venv_intel64 -f ./environments/mario_venv_x64.yaml
     conda activate mario_venv_intel64
-    echo "'mario_venv_intel64' successfully created and activated."
+    echo
+    echo "'mario_venv_intel64' successfully created."
+    echo
+    echo "You can now activate the environment by running 'conda activate mario_venv_intel64'"
+    echo
 # Check if architecture is Apple Silicon
 elif [ "$architecture" == "arm64" ]; then
     echo "Detected Apple Silicon architecture. Creating Apple Silicon conda environment."
+    echo
     conda env create -n mario_venv_arm64 -f ./environments/mario_venv_arm64.yaml
     conda activate mario_venv_arm64
+    echo
     echo "'mario_venv_arm64' successfully created and activated."
+    echo
+    echo "You can now activate the environment by running 'conda activate mario_venv_arm64'"
+    echo
 else
     echo "Unknown architecture: $architecture"
+    echo
 fi
